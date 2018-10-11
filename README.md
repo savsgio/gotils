3362f418bd8076c2ded22ebf0fa6baddd0f7c2fb
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2018-10-11 12:32:56 +0200

    Added travis CI

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 gotils
 ======
 
+[![Build Status](https://travis-ci.org/savsgio/gotils.svg?branch=master)](https://travis-ci.org/savsgio/gotils)
 [![Go Report Card](https://goreportcard.com/badge/github.com/savsgio/gotils)](https://goreportcard.com/report/github.com/savsgio/gotils)
 [![GoDoc](https://godoc.org/github.com/savsgio/gotils?status.svg)](https://godoc.org/github.com/savsgio/gotils)
 
---
+++ conv_test.go
@@ -0,0 +1,44 @@
+package gotils
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_B2S(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := struct {
+		args args
+		want string
+	}{
+		args: args{
+			b: []byte("Test"),
+		},
+		want: "Test",
+	}
+
+	if got := B2S(tests.args.b); got != tests.want {
+		t.Errorf("b2s(): '%v', want: '%v'", got, tests.want)
+	}
+}
+
+func Test_S2B(t *testing.T) {
+	type args struct {
+		b string
+	}
+	tests := struct {
+		args args
+		want []byte
+	}{
+		args: args{
+			b: "Test",
+		},
+		want: []byte("Test"),
+	}
+
+	if got := S2B(tests.args.b); !bytes.Equal(got, tests.want) {
+		t.Errorf("b2s(): '%v', want: '%v'", got, tests.want)
+	}
+}
