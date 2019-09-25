524bc4f475005d9dbedc1bc77248a07aef318ef0
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2019-09-25 09:07:55 +0200

    Add nocopy

---
+++ nocopy/nocopy.go
@@ -0,0 +1,14 @@
+package nocopy
+
+// NoCopy embed this type into a struct, which mustn't be copied,
+// so `go vet` gives a warning if this struct is copied.
+//
+// See https://github.com/golang/go/issues/8005#issuecomment-190753527 for details.
+// and also: https://stackoverflow.com/questions/52494458/nocopy-minimal-example
+type NoCopy struct{}
+
+// Lock ...
+func (*NoCopy) Lock() {}
+
+// Unlock ...
+func (*NoCopy) Unlock() {}
