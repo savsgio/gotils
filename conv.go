92b71a17f2608fa59d1b036976e4ecbc04f02789
Johannes Lauinger <johannes@lauinger-it.de>
2020-06-03 15:36:57 +0200

    update conv.go to use simpler, yet still safe, casts

--- conv.go
+++ conv.go
@@ -2,7 +2,6 @@ package gotils
 
 import (
 	"reflect"
-	"runtime"
 	"unsafe"
 )
 
@@ -12,30 +11,18 @@ import (
 // Note it may break if string and/or slice header will change
 // in the future go versions.
 func B2S(b []byte) string {
-	s := ""
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	sh.Data = bh.Data
-	sh.Len = bh.Len
-	
-	runtime.KeepAlive(b)
-	
-	return s
+    return *(*string)(unsafe.Pointer(&b))
 }
 
 // S2B converts string to a byte slice without memory allocation.
 //
 // Note it may break if string and/or slice header will change
 // in the future go versions.
-func S2B(s string) []byte {
-	b := make([]byte, 0)
+func S2B(s string) (b []byte) {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	bh.Data = sh.Data
 	bh.Cap = sh.Len
 	bh.Len = sh.Len
-	
-	runtime.KeepAlive(s)
-	
 	return b
 }
